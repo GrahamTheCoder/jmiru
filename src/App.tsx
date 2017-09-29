fd7d1371e77d4f124cece49eff1235b070ece379
GrahamTheCoder <grahamthecoder@gmail.com>
2017-09-29 13:34:54 +0100

    Tweak icon and name to match the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2><span className="App-logo">見</span><span className="App-title">Welcome to JMiru</span></h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          
         </p>
       </div>
     );
