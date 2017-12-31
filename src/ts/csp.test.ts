45ad9a01e4f39c5929595c8c30a5a94df80be8a1
GrahamTheCoder <grahamthecoder@gmail.com>
2017-12-31 14:35:56 +0000

    Failing test

--- src/ts/csp.test.ts
+++ src/ts/csp.test.ts
@@ -1,17 +1,42 @@
+import { isKanji } from 'wanakana';
 import { MatchFuriganaForLine } from './csp';
 
-it('Works with no spaces in input', () => {
-    const japanese = '容赦ないねいつの間に見失ったルート、暴れだす';
-    const hiragana = 'ようしゃないねいつのまにみうしなったるうと、あばれだす';
-    const expected = '容[よう]赦[しゃ]ないねいつの間[ま]に見[みう]失[しな]ったルート、暴[あば]れだす';
-  const solved = MatchFuriganaForLine(japanese, hiragana);
-    const actual = solved.map(v => {
-        let suffix = v.trailingUnmatched;
-        if (v.shouldDisplay) {
-          suffix = '[' + v.kana /*+ ',' + v.debug*/ + ']' + suffix;
-        }
-        return v.japanese + suffix;
-      }).join('');
-    expect(actual).toBe(expected);
+function expectSolveForLineToBe(expected: string) {
+  let inBrackets = false;
+  let japanese = '';
+  let hiragana = '';
+  expected.split('').map(c => {
+    if (c === '[') {
+      inBrackets = true;
+    } else if (c === ']') {
+      inBrackets = false;
+    } else if (inBrackets) {
+      hiragana += c;
+    } else {
+      japanese += c;
+      if (!isKanji(c)) {
+        hiragana += c;
+      }
+    }
   });
-  
+  const solved = MatchFuriganaForLine(japanese, hiragana);
+  const actual = solved.map(v => {
+      let suffix = v.trailingUnmatched;
+      if (v.shouldDisplayDebug) {
+        suffix = '[' + v.kana /*+ ',' + v.debug*/ + ']' + suffix;
+      }
+      return v.japanese + suffix;
+    }).join('');
+
+  expect(actual).toBe(expected);
+}
+
+describe('Works with no spaces in input', () => {
+  test('With kanji always matching 2 kana', () => {
+    expectSolveForLineToBe('容[よう]赦[しゃ]ないねいつの間[ま]に見[みう]失[しな]ったルート、暴[あば]れだす');
+  });
+
+  test('With kanji matching 1 or 3 kana', () => {
+    expectSolveForLineToBe('心[こころ]が体[からだ]を追[お]い越[こ]してきたんだよ');
+  });
+});
